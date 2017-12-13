7573749185d2240d39fdd17f5f929cd11316f180
madhank93 <maddymadhan946@gmail.com>
2017-12-14 00:18:25 +0530

    Update

--- Core-Java/src/arrays/LengthVSLength.java
+++ Core-Java/src/arrays/LengthVSLength.java
@@ -8,7 +8,14 @@ public class LengthVSLength {
 		// It's used to represent size of array, in case of multidimensional array it represents base size not total size
 		int[] a = new int[10];
 		System.out.println("Length of an Array: "+a.length); // Output : 10
-
+		System.out.println();
+		
+		// Multi dimensional array
+		int [][] mul = new int [6][10];
+		System.out.println("Base size of an Array: "+mul.length);
+		System.out.println("Size of an Array at 0th position: "+mul[0].length);
+		System.out.println();
+		
 		
 		// length() - is a final method it's applicable only for String
 		// It represents number of characters present in the String
