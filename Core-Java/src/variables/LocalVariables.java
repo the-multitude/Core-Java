3576bd9cf5042243e871e3a4c9b7e1ba454262e3
madhank93 <maddymadhan946@gmail.com>
2017-12-15 04:24:29 +0530

    Initial commit.

--- Core-Java/src/variables/LocalVariables.java
+++ Core-Java/src/variables/LocalVariables.java
@@ -3,16 +3,16 @@ package variables;
 /**
 Local Variable:
 
-* To meet the temporary requirement local variables are created. Local variables are created inside a method
+ * To meet the temporary requirement local variables are created. Local variables are created inside a method
     or block or constructor.
 
-* Local variables are created when executing that block of code and destroyed once the block gets completed.
+ * Local variables are created when executing that block of code and destroyed once the block gets completed.
      Scope of the variable is exactly same as that block.
 
-* For local variables, JVM won't provide default values, we should provide the values explicitly before using
+ * For local variables, JVM won't provide default values, we should provide the values explicitly before using
     that variable
 
-
+* Only applicable modifier for local variable is "final". Otherwise it will throw compile error - illegal start of expression 
 
  */
 
@@ -20,16 +20,20 @@ Local Variable:
 public class LocalVariables {
 
 	public static void main(String[] args) {
-		
+
 		int i = 0;
-		
+		int x;
 		for (int j=0; j< 3; j++) {
-			
+
 			i = i+j;
 		}
+
+		// System.out.println(i +" "+j ); // Compile Error -  cannot find symbol symbol: variable j location: class LocalVariables
+
+		//System.out.println(x); // Compile Error - variable x might not have been initialized
 		
-		// System.out.println(i +"        "+j ); // Compile Error -  cannot find symbol symbol: variable j location: class LocalVariables
-																					
+		
+		
 	}
 
 }
---
+++ Core-Java/src/variables/UninitializedArray.java
@@ -0,0 +1,35 @@
+package variables;
+
+public class UninitializedArray {
+
+	int[] notInit;
+	int[] init = new int[5];
+
+
+	public static void main(String[] args) {
+
+		UninitializedArray tmp = new UninitializedArray();
+
+		System.out.println(tmp.notInit); // Output: null
+		System.out.println(tmp.init); // Output: [I@XXXXXX
+
+		//System.out.println(tmp.notInit[0]); // R.E - Exception in thread "main" java.lang.NullPointerException
+		System.out.println(tmp.init[0]); // Output: 0
+
+		/*
+		 
+		 int[] notInit -> array reference is created but not initialized. So JVM provides null value
+		 int[] init = new int[5]; -> Here with new operator we are an object because of that JVM provide
+		 							 default value '0'
+
+		 * Once an array is created and initialized, JVM provide default values to static array or instance array
+		    or local array.
+
+
+		 */
+
+
+
+	}
+
+}
