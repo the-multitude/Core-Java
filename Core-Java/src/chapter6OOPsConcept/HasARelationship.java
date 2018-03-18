94af62656ab833d56cd541c191c254542ddc395e
madhank93 <maddymadhan946@gmail.com>
2018-03-18 18:53:49 +0530

    Typo

---
+++ Core-Java/src/chapter6OOPsConcept/HasARelationship.java
@@ -0,0 +1,27 @@
+package chapter6OOPsConcept;
+
+/*
+ Has-A-Relationship:
+   
+  * Has-A-Relationship is also known as "Aggregation" or "Composition"
+  
+  * There is no specific keyword to implement has-a-relationship, but mostly "new" keyword is used.
+    
+ */
+
+
+class Engine {
+	
+}
+
+class Car{
+	
+	Engine e = new Engine(); // Class car has engine reference.	
+	
+}
+
+public class HasARelationship {
+
+	Car cr = new Car(); // Class HasARelationship has car reference.
+	
+}
