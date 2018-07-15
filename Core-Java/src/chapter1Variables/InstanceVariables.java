5119b1c17c8e94e4ad25ec7b619bf78b5fbf73c2
madhank93 <maddymadhan946@gmail.com>
2018-07-15 17:04:42 +0530

    Typo changes on July 15th

--- Core-Java/src/chapter1Variables/InstanceVariables.java
+++ Core-Java/src/chapter1Variables/InstanceVariables.java
@@ -1,29 +1,31 @@
 package chapter1Variables;
 
 /**
+ * 
+ * @author Madhan
+ * 
  * Instance Variable:
-
- * If the value of a variable varied from object to object such types of variables are 
-	  called "Instance Variables"
-
- * For every object a separate copy of instance variable will be created.So performing any changes to 
-     instance variables, it will not be reflected to other objects.
-
- * The scope of instance variables is same as the scope of the objects, because instance variable
-	  will be created at the time of object creation and destroyed at the time of objects destruction
-
- * Instance variable should be declared within the class but outside of any method or block or constructor
-
- * Instance variable cannot be accessed from static area directly but it can be accessed by object reference
-
- * From instance area we can directly access instance members
-
- * For instance variable it is not required to initializes explicitly, JVM will provide default values
-
+ *	
+ * 		If the value of a variable varied from object to object such types of variables are 
+ *	  	called "Instance Variables"
+ *
+ * 		For every object a separate copy of instance variable will be created.So performing any changes to 
+ *    	instance variables, it will not be reflected to other objects.
+ *
+ * 		The scope of instance variables is same as the scope of the objects, because instance variable
+ *	  	will be created at the time of object creation and destroyed at the time of objects destruction
+ *
+ * 		Instance variable should be declared within the class but outside of any method or block or constructor
+ *
+ * 		Instance variable cannot be accessed from static area directly but it can be accessed by object reference
+ *
+ * 		From instance area we can directly access instance members
+ *
+ * 		For instance variable it is not required to initializes explicitly, JVM will provide default values
+ *
  */
 
 public class InstanceVariables {
-
 
 	int x = 10;
 	String s;
--- Core-Java/src/chapter1Variables/LocalVariables.java
+++ Core-Java/src/chapter1Variables/LocalVariables.java
@@ -1,19 +1,23 @@
 package chapter1Variables;
 
 /**
+ * 
+ * @author Madhan
+ * 
  * Local Variable:
-
- * To meet the temporary requirement local variables are created. Local variables are created inside a method
-    or block or constructor.
-
- * Local variables are created when executing that block of code and destroyed once the block gets completed.
-     Scope of the variable is exactly same as that block.
-
- * For local variables, JVM won't provide default values, we should provide the values explicitly before using
-    that variable
-
-* Only applicable modifier for local variable is "final". Otherwise it will throw compile error - illegal start of expression 
-
+ *
+ * 		To meet the temporary requirement local variables are created. Local variables are created inside a method
+ *   	or block or constructor.
+ *
+ * 		Local variables are created when executing that block of code and destroyed once the block gets completed.
+ *   	Scope of the variable is exactly same as that block.
+ *
+ * 		For local variables, JVM won't provide default values, we should provide the values explicitly before using
+ *   	that variable
+ *
+ * 		Only applicable modifier for local variable is "final". Otherwise it will throw compile error - illegal start
+ * 		of expression 
+ *
  */
 
 
@@ -31,9 +35,9 @@ public class LocalVariables {
 		// System.out.println(i +" "+j ); // Compile Error -  cannot find symbol symbol: variable j location: class LocalVariables
 
 		//System.out.println(x); // Compile Error - variable x might not have been initialized
-		
-		
-		
+
+
+
 	}
 
 }
