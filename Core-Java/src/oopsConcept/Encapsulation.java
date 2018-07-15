db3b4ac356484e835b4ffa262f7c77efdd8788e4
madhank93 <maddymadhan946@gmail.com>
2018-07-15 16:39:00 +0530

    Revert "Typo"
    This reverts commit 94af62656ab833d56cd541c191c254542ddc395e.

---
+++ Core-Java/src/oopsConcept/Encapsulation.java
@@ -0,0 +1,31 @@
+package oopsConcept;
+
+/**
+ Encapsulation:
+ 	Encapsulation is a mechanism of wrapping the data (variables) and code acting on the data (methods)
+ together as a single unit.
+
+ 				Encapsulation = Data Hiding + Abstraction
+ *To achieve encapsulation:
+
+	->	Declare the variables of a class as private.
+
+	->	Provide public setter and getter methods to modify and view the variables values.
+
+ */
+
+
+
+public class Encapsulation {
+
+	private double balanceAmount = 540000.00;
+
+	public double getBalanceAmount() {
+		return balanceAmount;
+	}
+
+	public void setBalanceAmount(double balanceAmount) {
+		this.balanceAmount = balanceAmount;
+	}
+
+}
